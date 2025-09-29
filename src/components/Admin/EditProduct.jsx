c6324e22354af0f4ad59f246520196942fbc1974
akshya <akshyathapa23@gmail.com>
2025-09-29 11:23:34 +0545

    admin product  to complete admin dashbord

--- src/components/Admin/EditProduct.jsx
+++ src/components/Admin/EditProduct.jsx
@@ -1,9 +1,169 @@
 import React from 'react'
+import { useState } from 'react'
+
+
 
 const EditProduct = () => {
+
+  const [productData, setProductData] = useState({
+    name:'',
+    discription:'',
+    prise: 0,
+    countInStock: 0,
+    sku:'',
+    category:'',
+    brand:'',
+    sizes:[],
+    colors:[],
+    collections:'',
+    meterial:'',
+    gender:'',
+    images:[
+      {
+        url:'https://picsum.photos/200?random=1',
+      },
+      {
+        url:'https://picsum.photos/200?random=2',
+      },
+    ]
+
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setProductData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+ const handleImageUpload = async (e) => {
+    const files = e.target.files[0];
+    console.log(files);
+
+    // const newImages = Array.from(files).map((file) => URL.createObjectURL(file));
+    // setProductData((prevData) => ({
+    //   ...prevData,
+    //   images: [...prevData.images, ...newImages],
+    // }));
+  };
+
+  const handleSumbit = (e) => {
+      e.preventDefault();
+      console.log(productData);
+
+  }
+
   return (
-    <div>
-      <h2>Edit Product</h2>
+    <div className=' max-w-5xl mx-auto p-6 shadow-md rounded-md'>
+      <h2 className='text-3xl font-bold mb-6'>Edit Product</h2>
+      <form onSubmit={handleSumbit}>
+        {/* Name */}
+        <div className='mb-6'>
+          <label className='block font-semibold mb-2'>Product Name </label>
+          <input
+            type='text'
+            name='name'
+            value={productData.name}
+            onChange={handleChange}
+            className=' w-full border border-gray-300 rounded-md p-2'
+            required
+          />
+        </div>
+        {/* Discription */}
+        <div className='mb-6'>
+          <label className='block font-semibold mb-2'>Discription</label>
+          <textarea
+            name='discription'
+            value={productData.discription}
+            onChange={handleChange}
+            className=' w-full border border-gray-300 rounded-md p-2'
+            required
+          />
+        </div>
+        {/* Price */}
+        <div className='mb-6'>
+          <label className='block font-semibold mb-2'>Price</label>
+          <input
+            type='number'
+            name='prise'
+            value={productData.prise}
+            onChange={handleChange}
+            className='w-full border border-gray-300 rounded-md p-2'
+            required
+          />
+        </div>  
+        {/* Count in Stock    */}
+        <div className='mb-6'>
+          <label className='block font-semibold mb-2'>Count in Stock</label>
+          <input
+            type='number'
+            name='countInStock'
+            value={productData.countInStock}
+            onChange={handleChange}
+            className='w-full border border-gray-300 rounded-md p-2'
+            required
+          />
+        </div> 
+        {/* SKU */}
+        <div className='mb-6'>
+          <label className='block font-semibold mb-2'>SKU</label>
+          <input
+            type='text'
+            name='sku'
+            value={productData.sku}
+            onChange={handleChange}
+            className='w-full border border-gray-300 rounded-md p-2'
+            required
+          />
+        </div> 
+        {/* Sizes */}
+        <div className='mb-6'>
+          <label className='block font-semibold mb-2'>Sizes (comma-seperated)</label>
+          <input
+            type='text'
+            name='sizes'
+            value={productData.sizes.join(', ')}
+            onChange={(e) => setProductData({ ...productData, sizes: e.target.value.split(',').map(size => size.trim())})}
+            className='w-full border border-gray-300 rounded-md p-2'
+            required
+          />
+        </div> 
+        {/* Color */}
+        <div className='mb-6'>
+          <label className='block font-semibold mb-2'>Colors (comma-seperated)</label>
+          <input
+            type='text'
+            name='colors'
+            value={productData.colors.join(', ')}
+            onChange={(e) => setProductData({ ...productData, colors: e.target.value.split(',').map((color) => color.trim())})}
+            className='w-full border border-gray-300 rounded-md p-2'
+            required
+          />
+        </div>
+        {/* Image Upload */}
+        <div className='mb-6'>
+          <label className='block font-semibold mb-2'>Image Upload</label>
+          <input
+            type='file'
+            name='image'
+            onChange={handleImageUpload}
+          />
+          <div className='flex gap-4 mt-4'>
+            {productData.images.map((image, index) =>(
+              <div key={index}>
+                <img src={image.url} alt='Product image' className='w-20 h-20 object-cover rounded-md shadow-md' />
+              </div>
+            )
+            )}
+
+          </div>
+        </div>
+        <button
+          type='submit'
+          className='bg-green-500 text-white w-full px-4 py-2 rounded hover:bg-green-600 transition-colors'>
+          Save Changes
+        </button>
+      </form>
     </div>
   )
 }
--- src/components/Admin/OrderManagement.jsx
+++ src/components/Admin/OrderManagement.jsx
@@ -1,9 +1,85 @@
 import React from 'react'
 
 const OrderManagement = () => {
+
+  const orders =[
+    {
+      _id: 123123,
+      user: {
+        name: 'akshya'
+      },
+      totalPrise: 5000,
+      status: 'Processing',
+    },
+    {
+      _id: 12355123,
+      user: {
+        name: 'Bipin'
+      },
+      totalPrise: 10000,
+      status: 'Processing',
+    }
+  ]
+
+
+ const handlestatuschange = (orderId, newStatus) => {
+  console.log(`Changing status for order ${orderId} to ${newStatus}`);
+ }
+
   return (
-    <div>
-      <h2>Order Management</h2>
+    <div className='max-w-7xl mx-auto p-6'>
+      <h2 className='text-2xl font-bold mb-6'>Order Management</h2>
+      <div className='overflow-x-auto shadow-md sm:rounded-lg'>
+        <table className='min-w-full text-gray-500'>
+          <thead className='bg-gray-200 text-xs uppercase text-gray-700 text-left'>
+            <tr>
+              <th className='px-4 py-2'>Order ID</th>
+              <th className='px-4 py-2'>Customer</th>
+              <th className='px-4 py-2'>Total Price</th>
+              <th className='px-4 py-2'>Status</th>
+              <th className='px-4 py-2'>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.length >0 ? (
+            orders.map((order) => (
+              <tr key={order._id} className='border-b hover:bg-gray-50 cursor-pointer text-left'>
+                <td className='p-4 font-medium text-gray-900 whitespace-nowrap'>#{order._id}</td>
+                <td className='p-4'>{order.user.name}</td>
+                <td className='p-4'>{order.totalPrise}</td>
+                <td className='p-4'>
+                  <select 
+                    value={order.status} 
+                    onChange={(e) => handlestatuschange(order._id, e.target.value)} 
+                    className='p-1 border rounded-lg'>
+                      <option value="Processing">Processing</option>
+                      <option value="Shipped">Shipped</option>
+                      <option value="Delivered">Delivered</option>
+                      <option value="Cancelled">Cancelled</option>
+                  </select>
+                </td>
+                <td className='p-4'>
+                  <button 
+                    onClick={() => handlestatuschange(order._id, 'Delivered')}
+                    className='bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 transition-colors'
+                    >
+                      Mark as Delivered
+                  </button>
+                </td> 
+                </tr>
+                  ))
+            ):
+            (
+              <tr>
+                <td colSpan="5" className="py-4 px-4 text-center text-gray-500">
+                  No Orders found.
+                </td>
+              </tr>
+            )
+          }
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
